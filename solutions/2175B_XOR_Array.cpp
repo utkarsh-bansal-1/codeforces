1b87aa2d02670eb96ffa51a2f30186ef58a85442
Utkarsh Bansal <utkarshbansal24march@gmail.com>
2026-08-09 02:50:44 +0530

    Sync Codeforces: 2175B - XOR Array [C++17 (GCC 7-32)]

--- solutions/2175B_XOR_Array.cpp
+++ solutions/2175B_XOR_Array.cpp
@@ -84,11 +84,10 @@ void solve() {
     for(int i=0 ; i<=n ; i++){
         b[i]=i;
     }
-    b[l-1]=1e8;
-    b[r]=1e8;
- 
+    
+    b[l-1]=b[r];
     for(int i=1 ; i<=n ; i++){
-        cout<<(b[i]^b[i-1])<<" ";
+        cout<<(b[i]^(b[i-1]))<<" ";
     }
     cout<<"\n";
 }
